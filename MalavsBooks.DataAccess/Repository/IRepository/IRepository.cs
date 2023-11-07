ee453db41c3434c10ef3ccba6c0dfa83d6bf4010
W0823309 <W0823309@ADL-A1201I-20>
2023-11-06 20:05:19 -0500

    Updated Part2Repository.cs

---
+++ MalavsBooks.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace MalavsBooks.DataAccess.Repository.IRepository
+{
+   public interface IRepository<T> where T : class
+    {
+        T Get(int id); // Retrive a category from the database by id 
+        //List of categories based on requirements 
+        IEnumerable<T> GetAll(
+            Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+            string includeProperties = null // useful for foreign key reference 
+         );
+
+        void Add(T entity); // to add an entity
+        void Remove(int id); // to remove an object or categoty
+        void Remove(T entity); // another way to remove an object 
+        void RemoveRange(IEnumerable<T> entity); // removes a complete range of entities
+    }
+}
---
+++ MalavsBooks.DataAccess/Repository/Repository.cs
@@ -0,0 +1,43 @@
+﻿using MalavsBooks.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace MalavsBooks.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        // modify the database w/ the db context
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, string includeProperties = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
